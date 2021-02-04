ed8236fca9e51e5b8ca2c4ec1fa9de3955676422
Bykhkalo <codit.bykhkalo@gmail.com>
2021-02-04 09:42:34 +0200

    model refactoring

--- app/src/main/java/com/bykhkalo/redditapiclient/model/RedditPost.kt
+++ app/src/main/java/com/bykhkalo/redditapiclient/model/RedditPost.kt
@@ -1,10 +1,16 @@
 package com.bykhkalo.redditapiclient.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "posts")
 data class RedditPost (
+
+	//current identifier for loading next page with after=name
+	@PrimaryKey
+	@SerializedName("name") val name : String,
+
 	@SerializedName("subreddit_id") val subreddit_id : String,
 
 	@SerializedName("id") val id : String,
@@ -16,15 +22,15 @@ data class RedditPost (
 	//Post content text
 	@SerializedName("title") val title : String,
 
-	//current identifier for loading next page with after=name
-	@SerializedName("name") val name : String,
+
 	@SerializedName("num_comments") val num_comments : Int,
 
 	//Creation date in timestamp
 	@SerializedName("created_utc") val created_utc : Int,
 
-	@SerializedName("thumbnail") val thumbnail : String,
-	@SerializedName("url_overridden_by_dest") val url_overridden_by_dest : String,
+	@SerializedName("thumbnail") val thumbnail : String?,
+	@SerializedName("url_overridden_by_dest") val url_overridden_by_dest : String?,
+	@SerializedName("url") val url : String,
 ) {
 	// to be consistent w/ changing backend order, we need to keep a data like this
 	var indexInResponse: Int = -1
